f08e4950071c321062a36d2779290757bf319d28
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2014-11-11 09:34:00 +0200

    Додано скрипт для  відображення цитати

--- gold-placers.js
+++ gold-placers.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+  addWords();
+});
+
+function addWords(){
 $("#adAllThis").on('click', function(event){
   event.preventDefault();
   console.log('1');
@@ -6,4 +11,8 @@ $("#adAllThis").on('click', function(event){
    var tvor = $("#new-sourse").val();
    var citata = $("#new-phrase").val();
 
-    $("#old-phrases").append();
+    $("#old-phrases").append("<p><h3 style='font-family:Times, serif; font-weight: 300'>"+citata
+                               +"</h2></p>" + "<p> <h4 style='font-style: italic; font-weight: 100'> &copy   " + author +"  &laquo" + tvor +"&raquo</h4></p>");
+  }
+ );
+}
