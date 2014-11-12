d09281098949ec04204c7ec2c51abdd4c9fbfead
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2014-11-13 01:17:03 +0200

    .

--- www/gold-placers.js
+++ www/gold-placers.js
@@ -11,8 +11,11 @@ $("#adAllThis").on('click', function(event){
    var tvor = $("#new-sourse").val();
    var citata = $("#new-phrase").val();
 
-    $("#old-phrases").append("<p><h3 style='font-family:Times, serif; font-weight: 300'>"+citata
-                               +"</h2></p>" + "<p> <h4 style='font-style: italic; font-weight: 100'> &copy   " + author +"  &laquo" + tvor +"&raquo</h4></p>");
+    $("#old-phrases").append(
+        "<p><h3 style='font-family:Times, serif; font-weight: 300'>"+citata
+        +"</h2></p>" + "<p> <h4 style='font-style: italic; font-weight: 100'> &copy   "
+        + author +"  &laquo" + tvor +"&raquo</h4></p>"
+   );
   }
  );
 }
